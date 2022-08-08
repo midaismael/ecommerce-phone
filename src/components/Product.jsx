d4e3b5cd7b0079f99183dd81b2f9beda2a260806
Ahmed Ismael <midaismael@gmail.com>
2022-08-08 19:08:59 +0200

    product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import DATA from '../DATA';
 
 const Product = () => {
-    return(
+
+    const cardItem = (item) => {
+        return (
+            <div className="card my-5 py-4" key={item.id} style={{ width: "25rem" }}>
+                <img src={item.img} className="card-img-top" alt={item.title} />
+                <div className="card-body text-center">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="lead">${item.price}</p>
+                    <NavLink to={`/products/${item.id}`} href="#" className="btn btn-outline-primary">Buy Now</NavLink>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+
         <div>
-            
+            <div className="contanier py-5">
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <h1>Product</h1>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+            <div className="contanier">
+                <div className="row justify-content-around">
+                    {DATA.map(cardItem)}
+                </div>
+            </div>
         </div>
     )
 }
