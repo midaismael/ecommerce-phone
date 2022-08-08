1b80bb9942d97f58ae2aa314025650d818f882bf
Ahmed Ismael <midaismael@gmail.com>
2022-08-08 15:59:46 +0200

    cartbtn

--- src/components/buttons/Cartbtn.jsx
+++ src/components/buttons/Cartbtn.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 const Cartbtn = () => {
     return(
-        <div>
-
-        </div>
+        <>
+        <NavLink to="/cart" className="btn btn-outline-primary ms-2">
+            <span className="fa fa-shopping-cart me-1"> Cart (0) </span>
+        </NavLink>
+        </>
     )
 }
 
