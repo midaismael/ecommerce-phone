a6c52a7ba3129c7a16e8cd91b4d2964c3bbd4485
Ahmed Ismael <midaismael@gmail.com>
2022-08-23 21:40:40 +0200

    updateroutes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import About from './components/About';
-import Product from './components/Product';
-import Contact from './components/Contact';
-import {Routes, Route, Navigate} from 'react-router-dom'; 
-import ProductDetail from './components/ProductDetail';
-import Cart from './components/Cart';
-import Checkout from './components/Checkout' 
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Product from "./components/Product";
+import Contact from "./components/Contact";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <>
-    <Header/>
-    <Home/>
-    <Routes>
-    <Route exact path="/" element={<Home/>} />
-    <Route exact path="/Products" element={<Product/>} />
-    <Route exact path="/Products/:id" element={<ProductDetail/>} />
-    <Route exact path="/cart" element={<Cart/>} />
-    <Route exact path="/checkout" element={<Checkout/>} />
-    <Route exact path="/about" element={<About/>} />
-    <Route exact path="/contact" element={<Contact/>} />
-    <Route exact path="/" element={<Navigate replace to="/home"/>} />
-    </Routes>
-    <Footer />
+      <Header />
+      <Routes>
+        <Route exact path="/Products" element={<Product />} />
+        <Route exact path="/Products/:id" element={<ProductDetail />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
