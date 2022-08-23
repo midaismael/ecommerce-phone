562bf3e634ebff257bb363caa947a811048beabc
Ahmed Ismael <midaismael@gmail.com>
2022-08-23 21:31:00 +0200

    home

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <>
     <Header/>
+    <Home/>
     <Routes>
     <Route exact path="/" element={<Home/>} />
     <Route exact path="/Products" element={<Product/>} />
